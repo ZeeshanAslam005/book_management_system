79f4399dd17b927fc8ec7dd62ceac375a2152365
Zeeshan Aslam <engg.zeeshan05@gmail.com>
2024-11-25 22:39:18 +0500

    roles added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,22 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+      :recoverable, :rememberable, :validatable,
+      :two_factor_authenticatable, # MFA
+      :jwt_authenticatable, jwt_revocation_strategy: JwtDenylist
+
+  ROLES = %w[admin manager customer].freeze
+
+  validates :role, inclusion: { in: ROLES }
+
+  def admin?
+    role == 'admin'
+  end
+
+  def manager?
+    role == 'manager'
+  end
+
+  def customer?
+    role == 'customer'
+  end
 end
